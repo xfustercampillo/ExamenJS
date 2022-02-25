d187756d8816530a9a61bea022ec0d252a420c6e
Your Name <xfusterc@esliceu.net>
2022-02-25 19:22:43 +0100

    commit 1

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
-function _id(ident){
+function _id(ident) {
     return document.getElementById(ident);
 }
 
-function factorial(){
-    
+function factorial() {
+    var entradaFactorial = _id("entradaFactorial").value;
+    var operacion = "";
+    var resultado = 1;
+
+    if (entradaFactorial < 0) {
+        alert('error');
+    } else {
+        for (var i = entradaFactorial; i > 0; i--) {
+            resultado *= i;
+            operacion += i;
+
+            if (i>1) operacion+="*";
+        } 
+        _id("resultatFactorial").innerHTML = resultado;
+        _id("operacionsFactorial").innerHTML = operacion;
+
+
+    }
+
 }
 
-function calculaIVA(){
-    
+function calculaIVA() {
+
 }
 
-function canviaFons(tipus){
+function canviaFons(tipus) {
+
+
+
     
 }
